1024a4852f7731d3f72c37858c8bc940d28809a2
Barima Effah <effa.barima@gmail.com>
2023-01-14 13:43:57 +0000

    fix storybook module resolution problem

--- src/components/box/box.tsx
+++ src/components/box/box.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { styled } from "stitches/stitches.config";
 
-const Box = React.forwardRef((props, ref) => {
-  return <div></div>;
+const StyledBox = styled("div", {
+  background: "red",
+  height: 100,
+  width: 500,
+  variants: {
+    position: {
+      relative: {
+        position: "relative",
+      },
+      absolute: {
+        position: "absolute",
+      },
+      fixed: {
+        position: "fixed",
+      },
+    },
+  },
+});
+
+type BoxProps = React.ComponentProps<typeof StyledBox>;
+
+const Box = React.forwardRef<HTMLDivElement, BoxProps>((props, ref) => {
+  return <StyledBox ref={ref} {...props}></StyledBox>;
 });
 
 Box.displayName = "Box";
