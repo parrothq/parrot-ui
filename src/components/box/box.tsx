19a28fecb0c5d77d56e39c150d94df92241b003d
Barima Effah <effa.barima@gmail.com>
2023-02-03 00:09:39 +0000

    finally got parcel to build - time to test publish to npm

--- src/components/box/box.tsx
+++ src/components/box/box.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { styled } from "stitches/stitches.config";
+import { styled } from "../../stitches/stitches.config";
 
 const StyledBox = styled("div", {
   background: "red",
